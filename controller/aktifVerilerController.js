35f3d56db450cd2bd396785efb49757d8e678459
femresirvan <femresirvan@gmail.com>
2020-12-17 02:25:14 +0300

    Routing islemleri yapıldı
    /aktif-veriler
    /gecmis-veriler
    /
    şeklinde route lokasyonları tanımlandı
    ejs dosyaları oluşturuldu

---
+++ controller/aktifVerilerController.js
@@ -0,0 +1,3 @@
+module.exports.aktifVeriler = (req,res,next) => {
+    res.render('aktifVeriler');
+}
--- routers/router.js
+++ routers/router.js
@@ -4,12 +4,13 @@ const router = express.Router();//
 const indexController = require('../controller/indexController');
 const notFoundController = require('../controller/notFoundController');
 // const realtimeDataController = require('../controller/realtimeDataController');
-const historyController = require('../controller/historyController');
+const historyController = require('../controller/gecmisVerilerController');
 const apiController = require('../controller/apiController');
 const jwtController = require('../controller/jwtController');
+const aktifVeriler = require('../controller/aktifVerilerController');
 
 router.get('/', indexController.renderIndex);
-router.get('/history', historyController.renderHistory);
+router.get('/gecmis-veriler', historyController.renderHistory);
 router.get('/api/data',apiController.getData);
 // router.get('/api/number/:number',apiController.getlastXData);
 // router.get('/api/id/:id/number/:number',apiController.getlastxDatawithinId);
@@ -18,5 +19,6 @@ router.get('/api/yangin/id/:id' ,apiController.getDangerData2);
 // router.get('/api/send-data' , jwtController.checkAuth,apiController.createData);
 router.get('/api/delete-data', jwtController.checkAuth,apiController.deleteData);
 router.post('/api/getToken',jwtController.getToken);
+router.get('/aktif-veriler',aktifVeriler.aktifVeriler);
 
 module.exports = router;
